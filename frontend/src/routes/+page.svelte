1c08f8c302cadbde7873e408e418843600925fca
vaishnav-mk <vaishnav240204@gmail.com>
2023-03-22 17:39:50 +0530

    minor frontend changes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-nocheck
+
 	import { fade, fly } from 'svelte/transition';
 
 	let files = [];
@@ -35,10 +37,19 @@
 			.then((res) => {
 				console.log(res);
 				response = res;
-				handleToasts(true);
+				handleToasts(true, {
+					message: 'Operation successful!',
+					type: 'success',
+					delay: 2000
+				});
 			})
 			.catch((err) => {
 				response.error = err.message;
+				handleToasts(true, {
+					message: err.message,
+					type: 'error',
+					delay: 2000
+				});
 			});
 	}
 
@@ -60,36 +71,37 @@
 		}
 	}
 
-	$: if (files.length > 20) {
-		files = files.slice(0, 20);
-		handleToasts(true, {
-			message: 'Maximum 20 images allowed',
-			type: 'error',
-			delay: 2000
-		});
+	function handleFiles(e) {
+		e.preventDefault();
+		response = {};
+		files = e.target.files;
+		if (files.length > 20) {
+			handleToasts(true, {
+				message: 'Maximum 20 files allowed',
+				type: 'error',
+				delay: 2000
+			});
+			let fileBuffer = [];
+			Array.prototype.push.apply(fileBuffer, files);
+			files = fileBuffer.slice(0, 20);
+		}
 	}
-
 </script>
 
-<navbar class="navbar bg-base-100">
+<navbar class="navbar bg-base-200 text-neutral-content mb-20">
 	<div class="flex-1">
 		<div class="btn normal-case text-xl">Image Classifier</div>
 	</div>
 	<div class="flex-none">
-		<button class="btn btn-square btn-ghost">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block w-5 h-5 stroke-current"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-				/></svg
-			>
-		</button>
+		<a href="https://github.com/vaishnav-mk/image-classification" target="_blank">
+			<button class="btn btn-square btn-ghost">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+					><path
+						d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+					/></svg
+				>
+			</button>
+		</a>
 	</div>
 </navbar>
 
@@ -118,7 +130,7 @@
 	<div class="mockup-window border bg-base-300 w-full max-h-4xl">
 		<div class="flex flex-col items-center justify-center gap-3 px-4 py-16 bg-base-200">
 			<div class="flex flex-col items-center justify-center gap-2">
-				<input type="file" bind:files multiple class="file-input" />
+				<input type="file" accept="image/*" multiple on:change={handleFiles} class="file-input" />
 				{#if files.length}
 					<input
 						type="text"
@@ -150,7 +162,7 @@
 		<div class="divider">
 			{#if response.results}
 				<div class="btn btn-info btn-xs">
-					{response.results.bike.count ?? 0} bikes and {response.results.car.count ?? 0} cars detected
+					{response.results.bike.count ?? 0} bike(s) and {response.results.car.count ?? 0} car(s) detected
 				</div>
 			{:else}
 				<div class={`btn btn-xs ${files.length ? 'btn-success' : ''}`}>
@@ -198,8 +210,9 @@
 								<button
 									class="btn btn-xs btn-error btn-circle absolute top-1 left-1"
 									on:click={() => {
-										files = Object.assign([], files);
-										files.splice(i, 1);
+										let fileBuffer = [];
+										Array.prototype.push.apply(fileBuffer, files);
+										files = fileBuffer.filter((f) => f.name !== file.name);
 									}}
 								>
 									<svg
@@ -226,7 +239,7 @@
 
 {#if toast.show}
 	<div class="toast" in:fly={{ y: 200, duration: 2000 }} out:fade>
-		<div class={`alert ${toast.type == "success" ? "alert-success" : "alert-error"}`}>
+		<div class={`alert ${toast.type == 'success' ? 'alert-success' : 'alert-error'}`}>
 			<div>
 				<span>{toast.message}</span>
 			</div>
